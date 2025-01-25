0c6fa54cad0222130c5850983ef18143c2134c01
Joaquin Mello <165310726+JoacoMell0@users.noreply.github.com>
2025-01-25 16:37:25 -0300

    Seguimiento Camara, Menu pausa, Menu Inicio

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Función para cargar el nivel de juego
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Mello"); // Cambia "NombreDeTuEscenaDeJuego" al nombre exacto de tu escena
+    }
+
+    // Función para salir del juego
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("Salir del juego"); // Esto se verá en el editor pero no afectará el juego compilado.
+    }
+}
